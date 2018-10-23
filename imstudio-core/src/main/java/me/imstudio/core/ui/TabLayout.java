66c2e4c7bf010182f842ee6596f46e932dff8f1e
nphau <nphau.itus@gmail.com>
2018-10-23 12:01:17 +0700

    f

--- imstudio-core/src/main/java/me/imstudio/core/ui/TabLayout.java
+++ imstudio-core/src/main/java/me/imstudio/core/ui/TabLayout.java
@@ -40,6 +40,22 @@ public class TabLayout extends android.support.design.widget.TabLayout {
         }
     }
 
+    public void setTextAllCap(boolean isEnable) {
+        try {
+            ViewGroup parent = (ViewGroup) getChildAt(0);
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                ViewGroup child = (ViewGroup) parent.getChildAt(i);
+                for (int i1 = 0; i1 < child.getChildCount(); i1++) {
+                    View view = child.getChildAt(i1);
+                    if (view instanceof TextView)
+                        ((TextView) view).setAllCaps(isEnable);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void setTypeface(AssetManager manager, String path) {
         try {
             ViewGroup parent = (ViewGroup) getChildAt(0);
